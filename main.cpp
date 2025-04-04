762cd26805cfdf16eb0b9cc72562f587eaa6876c
Drew Davis <ddavis@srepa2.coop>
2025-04-04 16:56:35 -0500

    Added some info about what the user will be doing and started developing the menu

--- main.cpp
+++ main.cpp
@@ -4,7 +4,33 @@
 #include <vector>
 #include <map>
 
+// Begin Function Declarations
+
+/* void LightTable ()
+{
+
+}
+
+/* int main_func ()
+{
+
+} */
+
+/* 
+    This will be built assuming the user has knowledge about the existing infrastructure, access to existing mapping, and about how taps work
+*/
+
 int main ()
 {
-    std::cout << "Testing output to console" << std::endl;
+    // Outputting message about the funciton and purpose of the calculation
+    std::cout << "\n\nThis program is written to provide caluclation for insertion of new taps into the existing fiber internet infrastructure provided by Singing River." << std::endl;
+    std::cout << "Please read some of the questions and prompts carefully so the calculations will be as exact as possible." << std::endl;
+
+    // Begin some sort of menu so the user can see what options there are and
+
+    std::cout << "\n\nPlease choose the coresponding number to the feature you'd like to perfrom: (IE: enter '2' in the terminal and press 'enter')" << std::endl;
+
+    std::cout << "\nFeature Menu: " << std::endl;
+    std::cout << "1. Tap insertion" << std::endl;
+    std::cout << "2. Light loss table" << std::endl;
 }
